ed73b4b0cc0f3accb18f75fc47a6601c1f67bd05
bacary <bacaryd33@gmail.com>
2021-01-05 10:02:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## EclipseGitGithub Java Project
---
+++ src/main/java/EclipseGitGithub/EclipseGitGithub/EclipseGitGithub/Application.java
@@ -0,0 +1,9 @@
+package EclipseGitGithub.EclipseGitGithub.EclipseGitGithub;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour Epsi 1");
+    }
+
+}
